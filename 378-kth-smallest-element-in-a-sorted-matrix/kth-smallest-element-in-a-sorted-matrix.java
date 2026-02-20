ae0762bd064ccdb78e6db88b76cbd75b63042878
yashcodec <yashchauhan102006@gmail.com>
2026-02-20 22:58:34 +0530

    Time: 16 ms (60.23%) | Memory: 51.9 MB (37.68%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -6,8 +6,6 @@ class Solution {
         int n = matrix.length;
         int[] arr = new int[n * n];
         int index = 0;
-        
-        // 2 for loops to store elements
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 arr[index++] = matrix[i][j];
