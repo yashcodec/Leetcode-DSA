ec06c40ede534156096f1e1675004c358d8cbc58
yashcodec <yashchauhan102006@gmail.com>
2026-03-11 23:13:29 +0530

    Time: 3 ms (41.76%) | Memory: 42.8 MB (92.29%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> set = new HashSet<>();
+        for (char c : sentence.toCharArray()) {
+        set.add(c);
+        }
+          return set.size() == 26;
+    }
+}
